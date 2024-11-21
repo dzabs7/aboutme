53b5a5c1829c882e22fce1a57dce512fa15fb339
unknown <sudiradiaz@gmail.com>
2024-11-21 14:35:37 +0700

    about me

---
+++ Script/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+    const loadingScreen = document.getElementById("loading-screen");
+    setTimeout(() => {
+        window.location.href = "aboutme.html";
+    }, 5000);
+});
